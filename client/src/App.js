1caf9d9fd60fc9e35b33c3aca8377dd62765c3b4
muhammadabdullahimdad <muhammadabdullahimdad@gmail.com>
2022-06-02 15:50:55 +0500

    no commit

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Home from "./pages/Home";
+import AddEdit from "./pages/AddEdit";
+import View from "./pages/View";
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <ToastContainer position="top-center" />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/addContact" component={AddEdit} />
+          <Route exact path="/update/:id" component={AddEdit} />
+          <Route exact path="/view/:id" component={View} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
